a38b9e5cdfc5829cdaf5963eff039a3da9a5fa98
Joni <jonathan.aguilera@redb.ee>
2021-04-26 21:43:42 -0300

    Validaciones login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { FormBuilder, Validators, FormControl, FormGroup  } from '@angular/forms';
 import { AlertController } from '@ionic/angular';
 
 @Component({
@@ -11,7 +12,39 @@ export class LoginPage implements OnInit {
 
   user: string;
   pass: string;
-  constructor(private auth: AuthService, private alertController: AlertController) { }
+  loginForm = this.formBuider.group({
+    correo: ['', 
+    [
+      Validators.required, 
+      Validators.maxLength(40),
+      Validators.pattern('^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$')
+    ]],
+    clave: ['', [
+    
+      Validators.required,
+      Validators.maxLength(20),
+      Validators.minLength(6)
+    ]]
+  })
+
+  get correo() {
+    return this.loginForm.get('correo')
+  }
+  get clave() {
+    return this.loginForm.get('clave')
+  }
+
+  public errorMessages = {
+    correo: [
+     { type: 'required', message: 'El correo es obligatorio' },
+     { type: 'pattern', message: 'Ingrese un correo válido' },
+     { type: 'maxlength', message: 'El correo no puede tener mas de 40 caracteres' } ],
+    clave: [
+     { type: 'required', message: 'La clave es obligatoria' },
+     { type: 'maxlength', message: 'La clave no puede tener mas de 20 caracteres' },
+     { type: 'minlength', message: 'La clave no puede tener menos de seis caracteres' } ],
+ }
+  constructor(private auth: AuthService, private formBuider: FormBuilder, private alertController: AlertController) { }
 
   ngOnInit() {
   }
@@ -19,11 +52,11 @@ export class LoginPage implements OnInit {
   async login() {
     let validation:any = await this.auth.loginUser(this.user, this.pass);
     if(validation == 1){
-      this.presentAlert();
+      this.noValidado();
     }
   }
 
-  async presentAlert() {
+  async noValidado() {
     const alert = await this.alertController.create({
       cssClass: 'my-custom-class',
       subHeader: 'Credenciales incorrectas',
